a56e377c698edac4a6ddd2c58a6aab7373db2112
Sabakan <enosena0809@gmail.com>
2020-10-28 17:06:19 +0900

    Server Create Now

---
+++ ハニカムキャンピング/Assets/Server.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp ;
+
+public class Server : MonoBehaviour {
+    private int count = 0; // click counter
+    private WebSocket ws;
+    void Start () {
+        var url = "ws://localhost:8080";
+        ws = new WebSocket(url);
+        ws.Connect();
+    }
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("clicked!");
+            count++;
+            ws.Send("clicked No. " + count.ToString());
+        }
+    }
+}
