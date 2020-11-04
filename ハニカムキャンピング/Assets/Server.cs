7f36932541849fea23dac03fef5eeae84268481b
h29i27 <h29i27@ed.cc.suzuka-ct.ac.jp>
2020-11-04 16:41:36 +0900

    merged websocket

--- ハニカムキャンピング/Assets/Server.cs
+++ ハニカムキャンピング/Assets/Server.cs
@@ -7,7 +7,7 @@ public class Server : MonoBehaviour {
     private int count = 0; // click counter
     private WebSocket ws;
     void Start () {
-        var url = "ws://localhost:8080";
+        var url = "ws://172.16.98.82:8080";
         ws = new WebSocket(url);
         ws.Connect();
     }
