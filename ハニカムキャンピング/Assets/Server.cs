8b39fe4f1b4553e64ac1fc81d6267a17466fa115
Sabakan <enosena0809@gmail.com>
2020-11-04 16:59:49 +0900

    WebSocket Creating now

--- ハニカムキャンピング/Assets/Server.cs
+++ ハニカムキャンピング/Assets/Server.cs
@@ -5,18 +5,28 @@ using WebSocketSharp ;
 
 public class Server : MonoBehaviour {
     private int count = 0; // click counter
-    private WebSocket ws;
+    public WebSocket ws;
     void Start () {
         var url = "ws://localhost:8080";
         ws = new WebSocket(url);
         ws.Connect();
+        ws.OnMessage += (sender , e) => ReceivTest(e.Data) ;
     }
     void Update () {
+       
+
+        
+
         if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("clicked!");
             count++;
             ws.Send("clicked No. " + count.ToString());
         }
+    }
+
+    public void ReceivTest(string text)
+    {
+        Debug.Log(text) ;
     }
 }
