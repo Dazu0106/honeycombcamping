2bf672cd309242024d914b534ea1a28431946e13
h29i27 <h29i27@ed.cc.suzuka-ct.ac.jp>
2020-11-06 18:17:18 +0900

    create playerobject

--- ハニカムキャンピング/Assets/Scripts/Server.cs
+++ ハニカムキャンピング/Assets/Scripts/Server.cs
@@ -5,18 +5,39 @@ using WebSocketSharp ;
 
 public class Server : MonoBehaviour {
     private int count = 0; // click counter
-    private WebSocket ws;
+    public WebSocket ws;
+    public GameObject player2,player3,player4;
+    private string text="";
     void Start () {
-        var url = "ws://172.16.98.82:8080";
+        var url = "ws://localhost:8080";
         ws = new WebSocket(url);
         ws.Connect();
+        ws.OnMessage += (sender , e) => ReceivTest(e.Data) ;
     }
     void Update () {
+       if(text!="") 
+       {
+           player2.transform.position+=new Vector3(10,0,0);
+           text="";
+       }
+
+        
+
         if (Input.GetMouseButtonDown(0))
         {
+            
             Debug.Log("clicked!");
             count++;
-            ws.Send("clicked No. " + count.ToString());
+            //ws.Send("clicked No. " + 
+            ws.Send(count.ToString());
         }
+    }
+
+    public void ReceivTest(string message)
+    {   text=message;
+        
+        
+        
+        Debug.Log(text) ;
     }
 }
