e5c5d2cc559e187d4fb8ca614a1d291c2c0f13b2
h29i27 <h29i27@ed.cc.suzuka-ct.ac.jp>
2020-11-11 14:16:45 +0900

    modified server.cs

--- ハニカムキャンピング/Assets/Scripts/Server.cs
+++ ハニカムキャンピング/Assets/Scripts/Server.cs
@@ -1,24 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using WebSocketSharp ;
+using WebSocketSharp;
+using UnityEngine.Tilemaps;
 
 public class Server : MonoBehaviour {
     private int count = 0; // click counter
     public WebSocket ws;
     public GameObject player2,player3,player4;
+    public Tilemap Tilemap;
+    private Color player2color,player3color,player4color;
     private string text="";
+
+
+   
+    void Awake ()
+        {   
+            player2color = Color.blue;
+            player3color = Color.green;
+            player4color = Color.yellow;
+
+            UpdateTileColor(player2,player2color);
+            UpdateTileColor(player3,player3color);
+            UpdateTileColor(player4,player4color);
+        }
+
+
     void Start () {
         var url = "ws://localhost:8080";
         ws = new WebSocket(url);
         ws.Connect();
         ws.OnMessage += (sender , e) => ReceivTest(e.Data) ;
+
     }
     void Update () {
        if(text!="") 
        {
-           player2.transform.position+=new Vector3(10,0,0);
-           text="";
+           
+            player2.transform.position+=new Vector3(0.8f,0,0);
+            player3.transform.position+=new Vector3(-0.4f,-0.6f);
+            player4.transform.position+=new Vector3(-0.4f,0.6f);
+            
+            UpdateTileColor(player2,player2color);
+            UpdateTileColor(player3,player3color);
+            UpdateTileColor(player4,player4color);
+            text="";
+           
        }
 
         
@@ -39,5 +66,13 @@ public class Server : MonoBehaviour {
         
         
         Debug.Log(text) ;
+    }
+
+    public void UpdateTileColor(GameObject player,Color playercolor)
+    {   //var tilemap = GetComponent<Tilemap>();//Tilemapを取得
+        Vector3Int currentPlayerTile = Tilemap.WorldToCell(player.transform.position);//現在いるTileの座標を取得
+        Tilemap.SetTileFlags(currentPlayerTile, TileFlags.None);//Tileのフラグをtrueに
+        Tilemap.SetColor( currentPlayerTile, playercolor );//Tileのcolorをplayercolorに変更
+        print(playercolor);
     }
 }
