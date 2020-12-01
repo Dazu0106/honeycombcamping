94df95b707e8c4da56d0896f851c98a9394aa6c9
h29i27 <h29i27@ed.cc.suzuka-ct.ac.jp>
2020-12-01 18:00:39 +0900

    Create Check around TIle function

--- ハニカムキャンピング/Assets/Scripts/Server.cs
+++ ハニカムキャンピング/Assets/Scripts/Server.cs
@@ -7,22 +7,22 @@ using UnityEngine.Tilemaps;
 public class Server : MonoBehaviour {
     private int count = 0; // click counter
     public WebSocket ws;
-    public GameObject player2,player3,player4;
+    public GameObject player1,player2,player3;
     public Tilemap Tilemap;
-    private Color player2color,player3color,player4color;
+    private Color player1color,player2color,player3color;
     private string text="";
 
 
    
     void Awake ()
         {   
-            player2color = Color.blue;
-            player3color = Color.green;
-            player4color = Color.yellow;
+            player1color = Color.blue;
+            player2color = Color.green;
+            player3color = Color.yellow;
 
+            UpdateTileColor(player1,player1color);
             UpdateTileColor(player2,player2color);
             UpdateTileColor(player3,player3color);
-            UpdateTileColor(player4,player4color);
         }
 
 
@@ -36,14 +36,16 @@ public class Server : MonoBehaviour {
     void Update () {
        if(text!="") 
        {
-           
-            player2.transform.position+=new Vector3(0.8f,0,0);
-            player3.transform.position+=new Vector3(-0.4f,-0.6f);
-            player4.transform.position+=new Vector3(-0.4f,0.6f);
+           if(CheckAroundTile(player1))
+            {
+                player1.transform.position+=new Vector3(0.8f,0,0);
+            }
+            player2.transform.position+=new Vector3(-0.4f,-0.6f);
+            player3.transform.position+=new Vector3(-0.4f,0.6f);
             
+            UpdateTileColor(player1,player1color);
             UpdateTileColor(player2,player2color);
             UpdateTileColor(player3,player3color);
-            UpdateTileColor(player4,player4color);
             text="";
            
        }
@@ -74,5 +76,27 @@ public class Server : MonoBehaviour {
         Tilemap.SetTileFlags(currentPlayerTile, TileFlags.None);//Tileのフラグをtrueに
         Tilemap.SetColor( currentPlayerTile, playercolor );//Tileのcolorをplayercolorに変更
         print(playercolor);
+    }
+    public bool CheckAroundTile(GameObject player)
+    {
+        Vector3Int uRPos = Tilemap.WorldToCell(player.transform.position+new Vector3(0.4f,0.6f)); //右斜め上のタイルの色を取得
+        Vector3Int uLPos = Tilemap.WorldToCell(player.transform.position+new Vector3(0.4f,-0.6f));//左斜め上
+        Vector3Int rPos = Tilemap.WorldToCell(player.transform.position+new Vector3(0.8f,0,0));//真右
+        Vector3Int lPos = Tilemap.WorldToCell(player.transform.position+new Vector3(-0.8f,0,0));//真左
+        Vector3Int dRPos = Tilemap.WorldToCell(player.transform.position+new Vector3(-0.4f,0.6f)); //右斜め下
+        Vector3Int dLPos = Tilemap.WorldToCell(player.transform.position+new Vector3(-0.4f,-0.6f));//左斜め下
+
+        Color uRC=Tilemap.GetColor(uRPos);
+        Color uLC=Tilemap.GetColor(uLPos);
+        Color rC=Tilemap.GetColor(rPos);
+        Color lC=Tilemap.GetColor(lPos);
+        Color dRC=Tilemap.GetColor(dRPos);
+        Color dLC=Tilemap.GetColor(dLPos);
+        if((uRC==Color.white || uLC==Color.white || rC==Color.white|| lC==Color.white || dRC==Color.white || dLC==Color.white))
+        {
+            return true;
+        }
+
+        return false;
     }
 }
