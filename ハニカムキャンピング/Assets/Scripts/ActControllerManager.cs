804d32b03a4ff0574562d1d75748b75d91ee30b4
Genki Kondo <robirabbit422@ezweb.ne.jp>
2020-12-03 18:00:28 +0900

    add click move

--- ハニカムキャンピング/Assets/Scripts/ActControllerManager.cs
+++ ハニカムキャンピング/Assets/Scripts/ActControllerManager.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ActControllerManager : MonoBehaviour
 {
     GameObject up_R, mid_R, down_R;
     GameObject up_L, mid_L, down_L;
-
+    
     GameObject clickedButton;
-    Vector2 Actclicked;
+    GameObject player;
+
+    public Vector3 direction;
+
 
     // Start is called before the first frame update
     void Start()
-    {
+    {   
         up_R = GameObject.Find("Right up");
         mid_R = GameObject.Find("Right middle");
         down_R = GameObject.Find("Right down");
@@ -21,7 +25,7 @@ public class ActControllerManager : MonoBehaviour
         mid_L = GameObject.Find("Left middle");
         down_L = GameObject.Find("Left down");
 
-        Actclicked = new Vector2(1.3f, 1.3f);
+        player = GameObject.Find("Bullet");
     }
 
     // Update is called once per frame
@@ -30,38 +34,67 @@ public class ActControllerManager : MonoBehaviour
         if(Input.GetMouseButtonDown(0)){
             clickedButton = null;
 
+            // ClickedSprites check
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit2D hit2d = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
  
             if (hit2d) {
                 clickedButton = hit2d.transform.gameObject;
-                //hit2d.transform.localScale = Actclicked;
             } 
  
+            // 右上
             if(clickedButton == up_R){
                 Debug.Log("Moved right up");
+                direction = new Vector3(0.4f,0.6f);
+                player.GetComponent<MovementController>().transform.position += direction;
+                player.GetComponent<MovementController>().UpdatePosition();
             }
             
+            // 右
             if(clickedButton == mid_R){
                 Debug.Log("Moved right middle");
+                direction = new Vector3(0.8f,0,0);
+                player.GetComponent<MovementController>().transform.position += direction;
+                player.GetComponent<MovementController>().UpdatePosition();
             }
 
+            // 右下
             if(clickedButton == down_R){
                 Debug.Log("Moved right down");
+                direction = new Vector3(0.4f,-0.6f);
+                player.GetComponent<MovementController>().transform.position += direction;
+                player.GetComponent<MovementController>().UpdatePosition();
             }
 
+            // 左上
             if(clickedButton == up_L){
                 Debug.Log("Moved left up");
+                direction = new Vector3(-0.4f,0.6f);
+                player.GetComponent<MovementController>().transform.position += direction;
+                player.GetComponent<MovementController>().UpdatePosition();
             }
 
+            // 左
             if(clickedButton == mid_L){
                 Debug.Log("Moved left middle");
+                direction = new Vector3(-0.8f,0,0);
+                player.GetComponent<MovementController>().transform.position += direction;
+                player.GetComponent<MovementController>().UpdatePosition();
             }
 
+            // 左下
             if(clickedButton == down_L){
                 Debug.Log("Moved left down");
+                direction = new Vector3(-0.4f,-0.6f);
+                player.GetComponent<MovementController>().transform.position += direction;
+                player.GetComponent<MovementController>().UpdatePosition();
             }
 
         }
     }
 }
+
+// ボタンクリックをInputSystemに紐づける方法がわからない → 直接directionを指定してやって動かしてみる? 
+// MovementControllerを継承すると Debug.Logが働かなくなる? (そもそもif文が動いてない可能性あり)
+
+// MonoBehaviourを継承したMovementControllerであるから動くんじゃないかわからん
