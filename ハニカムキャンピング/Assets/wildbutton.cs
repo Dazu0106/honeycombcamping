0d980b5cf7a8d910f6dfaac95fe1d3025ee040e0
h29i35 <h29i42@ed.cc.suzuka-ct.ac.jp>
2020-12-09 13:32:28 +0900

    wildbutton change

--- ハニカムキャンピング/Assets/wildbutton.cs
+++ ハニカムキャンピング/Assets/wildbutton.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using WebSocketSharp;
 
 public class wildbutton : MonoBehaviour
 {
+    public int i;
     // Start is called before the first frame update
     void Start()
     {
-        
+        var url = "ws://localhost:8080";
+        ws = new WebSocket(url);
+        ws.Connect();
+        ws.OnMessage += (sender , e) => ReceivTest(e.Data) ;
+        i=0;
     }
 
     // Update is called once per frame
@@ -18,6 +24,15 @@ public class wildbutton : MonoBehaviour
 
     public void Onclick()
     {
-        Debug.Log("tamesi");
+        if(i==0){
+            Debug.Log("tamesi");
+            ws.Send("order");
+            i=i+1;
+        }
+    }
+
+    public void ReceivTest(string message)
+    {   
+        text=message;
     }
 }
