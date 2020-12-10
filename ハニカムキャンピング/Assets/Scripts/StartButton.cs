2ca67dc6f206ceebca582a68ae07360cbd472cb3
Sabakan <enosena0809@gmail.com>
2020-12-10 17:58:16 +0900

    titlescene creating

---
+++ ハニカムキャンピング/Assets/Scripts/StartButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using WebSocketSharp;
+
+
+public class StartButton : MonoBehaviour
+{
+
+    public WebSocket ws ;
+    public string message  ;
+    public string text = "" ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var url = "ws://172.16.98.82:8080" ;
+        ws = new WebSocket(url) ;
+        ws.Connect() ;
+        ws.OnMessage += (sender , e) => TitleReceive(e.Data) ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnclickStartButton()
+    {
+        SceneManager.LoadScene("Scene_Game") ;
+        ws.Send("ready") ;
+    }
+
+    public void TitleReceive(string message)
+    {
+        text = message ;
+    }
+}
