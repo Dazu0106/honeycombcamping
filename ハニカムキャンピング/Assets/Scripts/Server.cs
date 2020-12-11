e1699e12266511849960281a223e42fcf0085271
SaltGear <h29i27@ed.cc.suzuka-ct.ac.jp>
2020-12-11 13:15:01 +0900

    modified server message

--- ハニカムキャンピング/Assets/Scripts/Server.cs
+++ ハニカムキャンピング/Assets/Scripts/Server.cs
@@ -28,7 +28,7 @@ public class Server : MonoBehaviour {
 
 
     void Start () {
-        var url = /*"ws://172.16.98.82:8080";*/"ws://localhost:8080";
+        var url = "ws://172.16.98.82:8080";//"ws://localhost:8080";
         ws = new WebSocket(url);
         ws.Connect();
         ws.OnMessage += (sender , e) => ReceivTest(e.Data) ;
@@ -36,30 +36,142 @@ public class Server : MonoBehaviour {
     }
     void Update () {
         //Debug.Log(directionMsg[0]+",0");
-       if(text==directionMsg[0]+",0") 
-       {
-           
-            player[0].transform.position+=new Vector3(0.8f,0,0);
-            player[1].transform.position+=new Vector3(-0.4f,-0.6f);
-            player[2].transform.position+=new Vector3(-0.4f,0.6f);
-            
+        if(text==directionMsg[0]+",0") 
+        {    
+            player[0].transform.position+=new Vector3(0.4f,0.6f,0);//プレイヤー0が右上に移動
             UpdateTileColor(player[0],playercolor[0]);
-            UpdateTileColor(player[1],playercolor[1]);
-            UpdateTileColor(player[2],playercolor[2]);
             text="";
-            
-           
-       } 
-       
+        }
+
+        if(text==directionMsg[1]+",0") 
+        {    
+            player[0].transform.position+=new Vector3(0.8f,0,0);//プレイヤー0が右に移動
+            UpdateTileColor(player[0],playercolor[0]);
+            text="";
+        }   
+        
+        if(text==directionMsg[2]+",0") 
+        {    
+            player[0].transform.position+=new Vector3(0.4f,-0.6f,0);//プレイヤー0が右下に移動
+            UpdateTileColor(player[0],playercolor[0]);
+            text="";
+        }
+        
+        if(text==directionMsg[3]+",0") 
+        {    
+            player[0].transform.position+=new Vector3(-0.4f,-0.6f,0);//プレイヤー0が左下に移動
+            UpdateTileColor(player[0],playercolor[0]);
+            text="";
+        }
+
+        if(text==directionMsg[4]+",0") 
+        {    
+            player[0].transform.position+=new Vector3(-0.8f,0,0);//プレイヤー0が左に移動
+            UpdateTileColor(player[0],playercolor[0]);
+            text="";
+        }
+
+        if(text==directionMsg[5]+",0") 
+        {    
+            player[0].transform.position+=new Vector3(-0.4f,0.6f,0);//プレイヤー0が左上に移動
+            UpdateTileColor(player[0],playercolor[0]);
+            text="";
+        }
+
 
         
+        if(text==directionMsg[0]+",1") 
+        {    
+            player[1].transform.position+=new Vector3(0.4f,0.6f,0);//プレイヤー1が右上に移動
+            UpdateTileColor(player[1],playercolor[1]);
+            text="";
+        }
 
-     
+        if(text==directionMsg[1]+",1") 
+        {    
+            player[1].transform.position+=new Vector3(0.8f,0,0);//プレイヤー1が右に移動
+            UpdateTileColor(player[1],playercolor[1]);
+            text="";
+        }   
+        
+        if(text==directionMsg[2]+",1") 
+        {    
+            player[1].transform.position+=new Vector3(0.4f,-0.6f,0);//プレイヤー1が右下に移動
+            UpdateTileColor(player[1],playercolor[1]);
+            text="";
+        }
+        
+        if(text==directionMsg[3]+",1") 
+        {    
+            player[1].transform.position+=new Vector3(-0.4f,-0.6f,0);//プレイヤー1が左下に移動
+            UpdateTileColor(player[1],playercolor[1]);
+            text="";
+        }
+
+        if(text==directionMsg[4]+",1") 
+        {    
+            player[1].transform.position+=new Vector3(-0.8f,0,0);//プレイヤー1が左に移動
+            UpdateTileColor(player[1],playercolor[1]);
+            text="";
+        }
+
+        if(text==directionMsg[5]+",1") 
+        {    
+            player[1].transform.position+=new Vector3(-0.4f,0.6f,0);//プレイヤー1が左上に移動
+            UpdateTileColor(player[1],playercolor[1]);
+            text="";
+        }
+
+
+
+        if(text==directionMsg[0]+",2") 
+        {    
+            player[2].transform.position+=new Vector3(0.4f,0.6f,0);//プレイヤー2が右上に移動
+            UpdateTileColor(player[2],playercolor[2]);
+            text="";
+        }
+
+        if(text==directionMsg[1]+",2") 
+        {    
+            player[2].transform.position+=new Vector3(0.8f,0,0);//プレイヤー2が右に移動
+            UpdateTileColor(player[0],playercolor[2]);
+            text="";
+        }   
+        
+        if(text==directionMsg[2]+",2") 
+        {    
+            player[2].transform.position+=new Vector3(0.4f,-0.6f,0);//プレイヤー2が右下に移動
+            UpdateTileColor(player[2],playercolor[2]);
+            text="";
+        }
+        
+        if(text==directionMsg[3]+",2") 
+        {    
+            player[2].transform.position+=new Vector3(-0.4f,-0.6f,0);//プレイヤー2が左下に移動
+            UpdateTileColor(player[2],playercolor[2]);
+            text="";
+        }
+
+        if(text==directionMsg[4]+",2") 
+        {    
+            player[2].transform.position+=new Vector3(-0.8f,0,0);//プレイヤー2が左に移動
+            UpdateTileColor(player[2],playercolor[2]);
+            text="";
+        }
+
+        if(text==directionMsg[5]+",2") 
+        {    
+            player[2].transform.position+=new Vector3(-0.4f,0.6f,0);//プレイヤー2が左上に移動
+            UpdateTileColor(player[2],playercolor[2]);
+            text="";
+        }
+            
     }
 
     public void ReceivTest(string message)
-    {   text=message;
-            
+    {   
+        text=message;
+        Debug.Log(text);        
     }
 
     public void UpdateTileColor(GameObject player,Color playercolor)
