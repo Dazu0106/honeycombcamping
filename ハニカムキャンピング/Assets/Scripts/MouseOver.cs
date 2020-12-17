f1b749fb7612eedac21a2ef43df8870a7b29d34f
SaltGear <h29i27@ed.cc.suzuka-ct.ac.jp>
2020-12-17 12:37:30 +0900

    attach MouseOver.cs

--- ハニカムキャンピング/Assets/Scripts/MouseOver.cs
+++ ハニカムキャンピング/Assets/Scripts/MouseOver.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class MouseOver : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class MouseOver : MonoBehaviour
 {
-    //Detect if the Cursor starts to pass over the GameObject
-    public void OnPointerEnter(PointerEventData pointereventData)
-    {
-        //Output to console the GameObject's name and the following message
-        Debug.Log("Cursor Entering " + name + " GameObject");
+    public Renderer rend;
+
+    void start(){
+        rend = GetComponent<Renderer>();
     }
 
-    //Detect when Cursor leaves the GameObject
-    public void OnPointerExit(PointerEventData pointereventData)
-    {
-        //Output the following message with the GameObject's name
-        Debug.Log("Cursor Exiting " + name + " GameObject");
+    public void OnMouseEnter(){
+        Debug.Log("MouseEnter " + rend.name);
+        rend.material.color = new Color32(242, 242, 242, 200);
     }
+
+    public void OnMouseExit(){
+        Debug.Log("MouseExit " + rend.name);
+        rend.material.color = Color.white;
+    }
+
 }
